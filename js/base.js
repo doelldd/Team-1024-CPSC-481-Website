1657db8b91546116d96d18c85f8ca42b0bd5cf6e
Dustin Doell <doelldd@gmail.com>
2019-05-22 19:35:10 -0600

    Added side nav and some content

---
+++ js/base.js
@@ -0,0 +1,21 @@
+/**
+ * Team 1024 JS
+ */
+
+var UI = {
+	scrollToSection: function( element, event ) {
+		if( event !== undefined ) { event.preventDefault(); }
+		var section = $( element ).attr( 'href' );
+		// Scroll to the top of the specified section
+		$( 'html, body' ).animate( {
+			scrollTop: $( section ).offset().top
+			}, 600
+		);
+	}
+};
+
+$(function() {
+	$( 'body' ).on( 'click', '[href^="#"]', function( event ) {
+		UI.scrollToSection( this, event );
+	});
+});
